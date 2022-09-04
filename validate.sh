a4ed4887bf56a8c8f696fd71d86632bf96cb6913
Terrain Duck <mathilda1337@tutanota.com>
2022-09-04 17:13:00 +0200

    Add some color to validate script

--- validate.sh
+++ validate.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
+
+Violet="\e[1;35m"
+EndColor="\e[0m"
+
+printf "$Violet\n\nPHP Codesniffer\n$EndColor"
 tools/codesniff.sh
+
+printf "$Violet\n\nPHP MD\n\n$EndColor"
 tools/phpmd.sh
+
+printf "$Violet\n\nPHPStan\n\n$EndColor"
 tools/phpstan.sh
 
