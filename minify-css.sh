9da2237d25b8f647a4267a5b67fe75305e013fb2
Terrain Duck <mathilda1337@tutanota.com>
2022-09-10 08:44:19 +0200

    Update CSS minify script to add responsive styles last

--- minify-css.sh
+++ minify-css.sh
@@ -4,9 +4,12 @@ rm public/css/styles.min.css
 
 for file in public/css/*;
 do
-    if [ $file != "public/css/styles.css" ] && [ $file != "public/css/colors" ]; then
+    if [ $file != "public/css/styles.css" ] && [ $file != "public/css/colors" ] && [ $file != "public/css/responsive.css" ]; then
         echo "Minifying $file";
         npx postcss $file >> public/css/styles.min.css;
     fi
 done;
+
+echo "Minifying $file";
+npx postcss public/css/responsive.css >> public/css/styles.min.css;
 
